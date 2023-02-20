76c8213e6095a4803ded0a56afbc360bd4fa73bd
Lucas de mello <lucas_mello6@hotmail.com>
2023-02-20 10:26:08 -0300

    chore: add remote config firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:app_movies/modules/splash/splash_module.dart';
+import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -9,6 +10,7 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  await FirebaseRemoteConfig.instance.fetchAndActivate();
   runApp(const MyApp());
 }
 
