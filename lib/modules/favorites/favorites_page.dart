82f3b16e6af15a63b4407456a97049e2ea8944b5
Lucas de mello <lucas_mello6@hotmail.com>
2023-02-21 12:35:21 -0300

    fix: alter favorites and movies page

--- lib/modules/favorites/favorites_page.dart
+++ lib/modules/favorites/favorites_page.dart
@@ -7,6 +7,7 @@ class FavoritesPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         title: const Text('Favorites page'),
       ),
       body: Container(),
