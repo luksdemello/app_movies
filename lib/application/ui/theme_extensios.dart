8bae65f2d465db75a310cf7627ce8f2cedeb2148
Lucas de mello <lucas_mello6@hotmail.com>
2023-02-21 12:00:03 -0300

    feat: add color extensions

---
+++ lib/application/ui/theme_extensios.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+extension ThemeExtension on BuildContext {
+  Color get colorRed => const Color(0xFFEa4335);
+  Color get colorOrange => const Color(0xFFFFBA49);
+  Color get colorGrey => const Color(0xFF9B9B9B);
+}
