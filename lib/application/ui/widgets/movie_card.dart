af70688ac8fedf8cc6125c7cccbd813432b7e040
Lucas de mello <lucas_mello6@hotmail.com>
2023-02-21 16:57:22 -0300

    feat: create movies card list

---
+++ lib/application/ui/widgets/movie_card.dart
@@ -0,0 +1,81 @@
+import 'package:app_movies/application/ui/app_movies_icons_icons.dart';
+import 'package:app_movies/application/ui/theme_extensios.dart';
+import 'package:flutter/material.dart';
+
+class MovieCard extends StatelessWidget {
+  const MovieCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 158,
+      height: 280,
+      child: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Material(
+                  elevation: 2,
+                  borderRadius: BorderRadius.circular(20),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    clipBehavior: Clip.antiAlias,
+                    child: Image.network(
+                      'https://upload.wikimedia.org/wikipedia/pt/6/63/Joker_%282019%29.jpg',
+                      width: 148,
+                      height: 184,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                const Text(
+                  'Coringa',
+                  style: TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.w600,
+                  ),
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
+                ),
+                Text(
+                  '2019',
+                  style: TextStyle(
+                    fontSize: 11,
+                    fontWeight: FontWeight.w300,
+                    color: context.colorGrey,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 90,
+            right: -10,
+            child: Material(
+              elevation: 5,
+              shape: const CircleBorder(),
+              clipBehavior: Clip.antiAlias,
+              child: SizedBox(
+                height: 30,
+                child: IconButton(
+                  iconSize: 13,
+                  icon: Icon(
+                    AppMoviesIcons.heart,
+                    color: context.colorGrey,
+                  ),
+                  onPressed: () {},
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
