891ecb8754b5de9941c9314918aa7f9933a54dc0
Lucas de mello <lucas_mello6@hotmail.com>
2023-02-21 12:34:08 -0300

    feat: add method logout in repository

--- lib/repositories/login/login_repository_impl.dart
+++ lib/repositories/login/login_repository_impl.dart
@@ -22,4 +22,10 @@ class LoginRepositoryImpl implements LoginRepository {
 
     throw Exception('Erro ao realizar login com google');
   }
+
+  @override
+  Future<void> logout() async {
+    await GoogleSignIn().signOut();
+    await FirebaseAuth.instance.signOut();
+  }
 }
