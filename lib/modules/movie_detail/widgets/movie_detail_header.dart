d3376482b40ca930324cdfe5224843fcd449a0b7
Lucas de mello <lucas_mello6@hotmail.com>
2023-02-22 09:47:21 -0300

    fix: add fade in images

--- lib/modules/movie_detail/widgets/movie_detail_header.dart
+++ lib/modules/movie_detail/widgets/movie_detail_header.dart
@@ -1,5 +1,6 @@
 import 'package:app_movies/models/movie_detail_model.dart';
 import 'package:flutter/material.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class MovieDetailHeader extends StatelessWidget {
   final MovieDetailModel? movie;
@@ -21,7 +22,10 @@ class MovieDetailHeader extends StatelessWidget {
             final image = movie!.urlImages[index];
             return Padding(
               padding: const EdgeInsets.all(2),
-              child: Image.network(image),
+              child: FadeInImage.memoryNetwork(
+                placeholder: kTransparentImage,
+                image: image,
+              ),
             );
           },
         ),
